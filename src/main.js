ad278665f9fcd10974936a891141e1cdfc1a6c37
Ravikiran Kalal <ravikiran.kalal@gmail.com>
2025-07-23 20:51:09 +0700

    Landing page

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Caspian</h1>
+    <p class="read-the-docs">
+      Your Vite project is ready! Start editing to see changes.
+    </p>
+  </div>
+`
+
+console.log('Caspian project running with Vite');
